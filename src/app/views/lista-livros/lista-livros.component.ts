8f129ce3848ad88be7fd4aedb9918797193fb849
Rodrigo Sundfeld <rodrigo.psundfeld@gmail.com>
2025-01-31 07:51:58 -0300

    better search

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Livro } from 'src/app/models/interfaces';
+import { FormControl } from '@angular/forms';
+import { map, Subscription, switchMap } from 'rxjs';
+import { Item, Livro } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -11,22 +12,29 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent implements OnDestroy {
   listaLivros: Livro[];
-  campoBusca: string = '';
+  campoBusca = new FormControl();
   subscription: Subscription;
   livro: Livro;
 
   constructor(private service: LivroService) {}
 
-  buscarLivros() {
-    this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (items) => this.listaLivros = this.livrosResultadoParaLivros(items),
-      error: (erro) => console.log(erro),
-      complete: () => console.log('Observable completo'),
-    });
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    switchMap((valorDigitado: string) => this.service.buscar(valorDigitado)),
+    map(
+      (items: Item[]) =>
+        (this.listaLivros = this.livrosResultadoParaLivros(items))
+    )
+  );
+
+  // buscarLivros() {
+  //   this.subscription = this.service.buscar(this.campoBusca).subscribe({
+  //     next: (items) => this.listaLivros = this.livrosResultadoParaLivros(items),
+  //     error: (erro) => console.log(erro),
+  //     complete: () => console.log('Observable completo'),
+  //   });
+  // }
 
   livrosResultadoParaLivros(items): LivroVolumeInfo[] {
-
     return items.map((item) => {
       return new LivroVolumeInfo(item.volumeInfo);
     });
